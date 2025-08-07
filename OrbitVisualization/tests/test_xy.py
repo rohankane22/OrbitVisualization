e3a03ba7c269112e779e36cdd4357b8bb59ec9a6
mattlastovka <matthew.lastovka@yahoo.com>
2025-08-07 12:30:24 -0400

    add code to run the test

--- OrbitVisualization/tests/test_xy.py
+++ OrbitVisualization/tests/test_xy.py
@@ -5,5 +5,8 @@ def test_xy():
     planet = ov.Planet(1,1,1)
     system = ov.System(star_dict={'M': 1, 'Teff': 5700},planet_dict={'1': planet})
 
-    assert planet.x == pytest.approx(-1, 1e-5) "initial position not as expected"
-    assert planet.y == pytest.approx(0, 1e-5) "initial position not as expected"
+    assert planet.x == pytest.approx(-1, 1e-5)  #"initial position not as expected"
+    assert planet.y == pytest.approx(0, 1e-5)  #"initial position not as expected"
+
+if __name__ == "__main__":
+    test_xy()
